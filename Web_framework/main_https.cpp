f62958986dee7c1ef7665230e011bf54f302c5da
wenmingxing1 <1793701192@qq.com>
2018-03-13 22:08:45 +0800

    更改注释

--- Web_framework/main_https.cpp
+++ Web_framework/main_https.cpp
@@ -5,7 +5,7 @@ using namespace Wenmingxing;
 
 int main()
 {
-    //HTTPS服务运行在12345端口，并启用四个线程,并以数字证书和私钥初始化
+    //HTTPS服务运行在12345端口，并启用四个线程,并以数字证书和密钥初始化
     Server<HTTPS> server(12345, 4, "server.crt", "server.key");
     start_server<Server<HTTPS>>(server);
 
